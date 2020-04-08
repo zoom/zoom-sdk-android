08193755798f809627aa1e05a6b424e96c53b768
Jonathan White <jwhite@thinkdigital.software>
2020-04-07 20:24:16 -0700

    Update AuthConstants.java
    Fix grammar

--- mobilertc-android-studio/sample/src/main/java/us/zoom/sdksample/initsdk/AuthConstants.java
+++ mobilertc-android-studio/sample/src/main/java/us/zoom/sdksample/initsdk/AuthConstants.java
@@ -2,20 +2,20 @@ package us.zoom.sdksample.initsdk;
 
 public interface AuthConstants {
 
-	// TODO Change it to your web domain
+	// TODO Change this to your web domain
 	public final static String WEB_DOMAIN = "zoom.us";
 
-	// TODO Change it to your APP Key
+	// TODO Change this to your APP Key
 	public final static String SDK_KEY = "";
 
-	// TODO Change it to your APP Secret
+	// TODO Change this to your APP Secret
 	public final static String SDK_SECRET = "";
 
 	/**
-	 * We recommend that, you can generate jwttoken on your own server instead of hardcore in the code.
-	 * We hardcore it here, just to run the demo.
+	 * We recommend that you generate a jwttoken on your own server instead of hardcoding it in your code.
+	 * We hardcode it here just for demonstration purposes.
 	 *
-	 * You can generate a jwttoken on the https://jwt.io/
+	 * You can generate a jwttoken at https://jwt.io/
 	 * with this payload:
 	 * {
 	 *     "appKey": "string", // app key
